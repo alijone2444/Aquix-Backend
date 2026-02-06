bddf6157c783b410e88c526aaf1f9e60258db75a
Ali Jone <alijone2444@gmail.com>
2026-02-06 08:11:29 +0500

    Investor page in admin is completed..

--- api/index.js
+++ api/index.js
@@ -2,7 +2,5 @@
 // This file wraps the Express app for Vercel's serverless environment
 const app = require('../src/server');
 
-// Export the Express app directly for Vercel
-// Vercel will automatically handle it as a serverless function
 module.exports = app;
 
